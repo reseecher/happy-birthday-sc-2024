ca93ffa1da20648b204f5e56dc70ab8c74480d2b
Resee03 <1635352357@qq.com>
2024-10-19 16:57:07 +0800

    del box

--- script/main.js
+++ script/main.js
@@ -27,12 +27,7 @@ const fetchData = () => {
 // Animation Timeline
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
-  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
-
-  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
     .split("")
